95af89b69d75dabd6246f49afa8b4a6c6d0748e5
RavenRock I.S - Tecnologia com propósito <corporativo@ravenrock.com.br>
2025-07-28 23:23:29 -0300

    Delete index.jsx

--- index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import RavenRockSite from './RavenRockSite';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><RavenRockSite /></React.StrictMode>);
